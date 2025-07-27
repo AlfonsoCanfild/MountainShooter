3b5b80f8d46b2ebc1f26b92fbc219d01b8c23a42
Alfonso <alfcanfild@gmail.com>
2025-07-27 08:02:03 -0300

    Fisrt commit.

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import pygame
 
-print('Setup Start')
+# pygame setup
 pygame.init()
-window = pygame.display.set_mode(size=(800, 580))
-print('Setup Start')
+screen = pygame.display.set_mode((740, 520))
+pygame.display.set_caption("Mountain Shooter - by Alfonso")
+clock = pygame.time.Clock()
+running = True
 
-while True:
-    # check for all events
+while running:
+    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            var = pygame.QUIT
-            quit()
+            running = False
+
+    # fill the screen with a color
+    screen.fill("black")
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
 
