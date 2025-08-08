ed01395895d153acce5beeba297f4c733a93673d
Alfonso <alfcanfild@gmail.com>
2025-08-08 07:24:21 -0300

    SQLite implementado com sucesso.

--- code/Game.py
+++ code/Game.py
@@ -2,7 +2,8 @@ import pygame
 
 from code.Level import Level
 from code.Menu import Menu
-from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION, FONT_TYPE, C_WHITE
+from code.Score import Score
 
 
 class Game:
@@ -10,8 +11,20 @@ class Game:
         pygame.init()
         self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
+    def show_phase_text(self, text):
+        # Carrega a imagem de fundo
+        background = pygame.image.load("assets/tela_2_fase.png").convert()
+        background = pygame.transform.scale(background, (WIN_WIDTH, WIN_HEIGHT))
+
+        # Desenha a imagem de fundo
+        self.window.blit(background, (0, 0))
+        pygame.display.update()
+
+        pygame.time.delay(2000)  # Espera  segundo
+
     def run(self):
         while True:
+            score = Score(self.window)
             menu = Menu()
             menu_return = menu.run()
 
@@ -19,13 +32,20 @@ class Game:
                 player_score = [0, 0]  # lista com Player1 e Player2
                 level = Level(self.window, 'Level1', menu_return, player_score)
                 level_return = level.run(player_score)
+                # 2° Fase
                 if level_return:
+                    self.show_phase_text("2ª Fase")
                     level = Level(self.window, 'Level2', menu_return, player_score)
                     level_return = level.run(player_score)
+                    if level_return:
+                        score.save(menu_return, player_score)
+
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
 
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()
                 quit()
             else:
-                pass
 
+                pass
---
+++ code/Score.py
@@ -0,0 +1,111 @@
+import sys
+from datetime import datetime
+
+import pygame
+from pygame import Surface, Rect, KEYDOWN, K_RETURN, K_BACKSPACE, K_ESCAPE
+from pygame.font import Font
+
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_SOUND, FONT_TYPE, C_ORANGE, SCORE_POS, \
+    MENU_OPTION, C_WHITE
+from code.DBProxy import DBProxy
+
+
+class Score:
+
+    def __init__(self, window):
+        self.window_size = (WIN_WIDTH, WIN_HEIGHT)
+        self.window = pygame.display.set_mode(self.window_size)
+        pygame.display.set_caption("Mountain Shooter - by Alfonso")
+        self.clock = pygame.time.Clock()
+
+        self.background = pygame.transform.scale(
+            pygame.image.load("assets/tela_inicial_sem_titulo.png"),
+            self.window_size
+        )
+
+    def save(self, game_mode: str, player_score: list[int]):
+        pygame.mixer.music.load(MENU_SOUND)
+        pygame.mixer.music.set_volume(0.5)
+        pygame.mixer.music.play(-1)
+        db_proxy = DBProxy('DBScore')
+        name = ''
+
+        while True:
+            self.window.blit(self.background, (0, 0))  # ← limpa a tela
+            self.score_text(45, 'YOU WIN!!!!', C_ORANGE, SCORE_POS['Title'])
+
+            if game_mode == MENU_OPTION[0]:
+                score = player_score[0]
+                text = 'Enter your name (4 chars):'
+            elif game_mode == MENU_OPTION[1]:
+                score = (player_score[0] + player_score[1]) / 2
+                text = 'Enter Team name (4 chars):'
+            elif game_mode == MENU_OPTION[2]:
+                if player_score[0] >= player_score[1]:
+                    score = player_score[0]
+                    text = 'Enter your name (4 chars):'
+                else:
+                    score = player_score[1]
+                    text = 'Enter your name (4 chars):'
+
+            self.score_text(25, text, C_WHITE, SCORE_POS['EnterName'])
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == KEYDOWN:
+                    if event.key == K_RETURN and len(name) == 4:
+                        db_proxy.save({'name': name, 'score': score, 'date': get_formatted_date()})
+                        self.show()
+                        return
+                    elif event.key == K_BACKSPACE:
+                        name = name[:-1]
+                    else:
+                        if len(name) < 4:
+                            name += event.unicode
+
+            self.score_text(32, name, C_WHITE, SCORE_POS['Name'])
+            pygame.display.flip()
+            self.clock.tick(60)
+
+    def show(self):
+        pygame.mixer.music.load(MENU_SOUND)
+        pygame.mixer.music.set_volume(0.5)
+        pygame.mixer.music.play(-1)
+
+        self.window.blit(self.background, (0, 0))
+        self.score_text(32, 'Top 10 Score', C_WHITE, SCORE_POS['Title'])
+
+        # Cabeçalho fixo com alinhamento por espaçamento
+        self.score_text(20, f"{' NAME':<10} {' SCORE':<8} {'  DATE':<10}", C_WHITE, SCORE_POS[
+            'Label'])
+
+        db_proxy = DBProxy('DBScore')
+        list_score = db_proxy.retrieve_top10()
+        db_proxy.close()
+
+        for i, player_score in enumerate(list_score):
+            id_, name, score, date = player_score
+            line = f"{name:<10} {int(score):05d}   {date}"
+            self.score_text(20, line, C_WHITE, SCORE_POS[i])
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == KEYDOWN and event.key == K_ESCAPE:
+                    return
+            pygame.display.flip()
+            self.clock.tick(60)
+
+    def score_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name=FONT_TYPE, size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
+
+
+def get_formatted_date():
+    return datetime.now().strftime("%d/%m/%y")
